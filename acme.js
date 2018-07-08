f4c9fdbfabcef391f8dd903922d8e10b4c2acdba
Tom Licitra <tom.licitra@gmail.com>
2018-07-07 23:03:20 -0400

    initial commit

---
+++ acme.js
@@ -0,0 +1,42 @@
+const customers = [
+    {
+        "id": 1,
+        "name": "Moe"
+    },
+    {
+        "id": 2,
+        "name": "Larry"
+    },
+    {
+        "id": 3,
+        "name": "Curly"
+    },
+    {
+        "id": 4,
+        "name": "Homer"
+    }
+];
+
+const sales = [
+    {
+        "customerID": 1,
+        "orderID": 1,
+        "total": 10
+    },
+    {
+        "customerID": 2,
+        "orderID": 2,
+        "total": 20
+    },
+    {
+        "customerID": 3,
+        "orderID": 3,
+        "total": 30
+    },
+    {
+        "customerID": 1,
+        "orderID": 4,
+        "total": 100
+    }
+];
+
